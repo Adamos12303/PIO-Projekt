436a674857b85116bdf8a248ed9cca44d3851b41
Krata2002 <markgrovel24@gmail.com>
2026-06-15 11:35:04 +0200

    Aktualizacja zgodnie z TO DO w pull request
    Dodanie:
    - Zasad gry
    - Wyświetlania sumy kart krupiera od początku gry
    
    Oraz refaktoryzacja kodu
    (Mam nadzieje że poprawnie)

--- ProjektPIO/src/model/Deck.java
+++ ProjektPIO/src/model/Deck.java
@@ -7,20 +7,14 @@ import java.util.List;
 public class Deck {
     private final List<Card> cards = new ArrayList<>();
     public Deck() {
-        for (Card.Suit suit : Card.Suit.values()) {
-            for (Card.Rank rank : Card.Rank.values()) {
-                cards.add(new Card(suit, rank));
-            }
-        }
+        for (Card.Suit suit : Card.Suit.values()) { for (Card.Rank rank : Card.Rank.values()) { cards.add(new Card(suit, rank)); } }
         shuffle();
     }
-    public void shuffle() {
-        Collections.shuffle(cards);
-    }
+
+    public void shuffle() { Collections.shuffle(cards); }
+
     public Card drawCard() {
-        if (cards.isEmpty()) {
-            return null;
-        }
+        if (cards.isEmpty()) { return null; }
         return cards.remove(cards.size() - 1);
     }
 }
